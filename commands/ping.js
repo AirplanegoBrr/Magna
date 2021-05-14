4b393d078bf0656ff9f1be9b83f892c80e3f3362
AirplaneGoBrr <mottlcompound@gmail.com>
2021-05-14 01:14:47 -0700

    Quick Fix

--- commands/ping.js
+++ commands/ping.js
@@ -3,13 +3,13 @@ module.exports = {
     description: 'ping command.',
     execute(client, Discord, message, guild) {
         const Embed = new Discord.MessageEmbed()
-        .setColor('#0099ff')
-        .setTitle(`🏓Latency is ${Date.now() - message.createdTimestamp}ms. API Latency is ${Math.round(client.ws.ping)}ms`)
-        //.setURL()
-        .setAuthor(message.author.username, message.author.avatarURL(), message.author.avatarURL())
-        //.setDescription("Server: " + guild.name + "\nServerID:" + guild.id + "\nMember Count:" + guild.memberCount)
-        .setTimestamp()
-        //.setFooter('Bot by !!AirplaneGoBrr!!#1613', 'https://cdn.discordapp.com/avatars/250029754076495874/bdf3953b71edb421776e43d24e7651fe.webp');
+            .setColor('#0099ff')
+            .setTitle(`🏓Latency is ${Date.now() - message.createdTimestamp}ms. API Latency is ${Math.round(client.ws.ping)}ms`)
+            //.setURL()
+            .setAuthor(message.author.username, message.author.avatarURL(), message.author.avatarURL())
+            //.setDescription("Server: " + guild.name + "\nServerID:" + guild.id + "\nMember Count:" + guild.memberCount)
+            .setTimestamp()
+            //.setFooter('Bot by !!AirplaneGoBrr!!#1613', 'https://cdn.discordapp.com/avatars/250029754076495874/bdf3953b71edb421776e43d24e7651fe.webp');
         //message.channel.send(`🏓Latency is ${Date.now() - message.createdTimestamp}ms. API Latency is ${Math.round(client.ws.ping)}ms`);
         message.channel.send(Embed);
     }
---
+++ commands/shop.js
@@ -0,0 +1,14 @@
+module.exports = {
+    name: 'shop',
+    description: 'shop command.',
+    execute(client, Discord, message, guild) {
+        const { owner } = require("../config.json");
+        if (!message.author.id == owner) {
+            message.channel.send("This command is in testing.");
+        } else {
+            var x = message.content.split(" ").slice(1).join(" ")
+            var y = message.content.split(" ")
+            message.channel.send("Hi")
+        }
+    }
+}
